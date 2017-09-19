4b0094c80ae75490ef4691bd08f5c2c6a3ce239f
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-09-19 23:03:35 +0000

    start roxygenization of the colorspace package -> goal: facilitate cooperation with Claus Wilke
    desaturate() as a first step, more will follow
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/colorspace@177 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/colorspace/R/desaturate.R
+++ pkg/colorspace/R/desaturate.R
@@ -1,4 +1,44 @@
-## desaturate colors (remove chroma in HCL space)
+#' Desaturate Colors by Chroma Removal in HCL Space
+#' 
+#' Transform a vector of given colors to the corresponding colors with chroma
+#' removed (collapsed to zero) in HCL space.
+#' 
+#' Given colors are first transformed to RGB (either using
+#' \code{\link[colorspace]{hex2RGB}} or \code{\link[grDevices]{col2rgb}}) and
+#' then to HCL (\code{\link[colorspace]{polarLUV}}).  In HCL, chroma is removed
+#' (i.e., collapsed to zero) and then the color is transformed back to a
+#' hexadecimal string.
+#' 
+#' @param col vector of any of the three kind of R colors, i.e., either a color
+#' name (an element of \code{\link[grDevices]{colors}}), a hexadecimal string
+#' of the form \code{"#rrggbb"} or \code{"#rrggbbaa"} (see
+#' \code{\link[grDevices]{rgb}}), or an integer \code{i} meaning
+#' \code{palette()[i]}.
+#' @param amount numeric specifying the amount of desaturation where \code{1}
+#' corresponds to complete desaturation, \code{0} to no desaturation, and
+#' values in between to partial desaturation.
+#' @return A character vector with (s)RGB codings of the colors in the palette.
+#' @seealso \code{\link[colorspace]{polarLUV}}, \code{\link[colorspace]{hex}}
+#' @keywords color
+#' @examples
+#' ## rainbow of colors and their desaturated counterparts
+#' rainbow_hcl(12)
+#' desaturate(rainbow_hcl(12))
+#' 
+#' ## convenience demo function
+#' wheel <- function(col, radius = 1, ...)
+#'   pie(rep(1, length(col)), col = col, radius = radius, ...) 
+#' 
+#' ## compare base and colorspace palettes
+#' ## (in color and desaturated)
+#' par(mar = rep(0, 4), mfrow = c(2, 2))
+#' ## rainbow color wheel
+#' wheel(rainbow_hcl(12))
+#' wheel(rainbow(12))
+#' wheel(desaturate(rainbow_hcl(12)))
+#' wheel(desaturate(rainbow(12)))
+#' @export desaturate
+
 desaturate <- function(col, amount = 1) {
   ## col has to be hex code, otherwise col2rgb is used
   if(is.character(col) &&
