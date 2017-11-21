d3448cdc6358ed31e5e93552016ac6ab1f0c3d2e
clauswilke <clauswilke@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-11-21 19:28:44 +0000

    New models to lighten/darken colors.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/colorspace@249 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/colorspace/R/adjust_hcl.R
+++ pkg/colorspace/R/adjust_hcl.R
@@ -124,6 +124,7 @@ lightdark <- function(col, amount = 0.1,
   return(col)
 }
 
+
 if(FALSE) {
 ## code
 library("colorspace")
@@ -241,4 +242,238 @@ if ( FALSE ) {
    cols <- multiverge_hcl(10,h=h,l=l,lmax=lmax)
    showcols(cols)
 
+}
+
+
+lightdark2 <- function(col, amount = 0.1,
+                       method = "relative", space = c("HLS", "HCL", "combined"), fixup = TRUE)
+{
+  ## method
+  space <- match.arg(space, c("HLS", "HCL", "combined"))
+  method <- match.arg(method, c("relative", "absolute"))
+  
+  ## number of colors
+  n <- max(c(length(col), length(amount)))
+  col <- rep_len(col, length.out = n)
+  amount <- rep_len(amount, length.out = n)
+  
+  ## col has to be hex code, otherwise col2rgb is used
+  if(is.character(col) &&
+     (all(substr(col, 1L, 1L) == "#") & all(nchar(col) %in% c(7L, 9L))))
+  {
+    ## extract alpha from hex (if any)
+    alpha <- substr(col, 8L, 9L)
+    ## retain only RGB in hex
+    col <- substr(col, 1L, 7L)
+    ## convert to colorspace::RGB
+    col <- hex2RGB(col)
+  } else {
+    col <- col2rgb(col, alpha = TRUE)
+    ## extract alpha values (if non-FF)
+    alpha <- format(as.hexmode(col[4L, ]), width = 2L, upper.case = TRUE)
+    alpha[alpha == "FF"] <- ""
+    ## retain only RGB
+    col <- RGB(t(col[1L:3L, ])/255)
+  }
+  
+  if (space == "HCL") {
+    ## *** darkening/lightening in HCL space ***
+    
+    ## convert to HCL and remove chroma
+    col <- as(col, "polarLUV")
+    ## fix-up extreme luminance cases
+    col@coords[, "L"] <- pmin(100, pmax(0, col@coords[, "L"]))
+    
+    ## transform luminance
+    Lold <- col@coords[, "L"]
+    col@coords[, "L"] <- if(method == "relative") {
+      (amount >= 0) * (100 - (100 - Lold) * (1 - amount)) +
+        (amount < 0) * Lold * (1 + amount)
+    } else {
+      Lold + amount * 100
+    }
+    col@coords[, "L"] <- pmin(100, pmax(0, col@coords[, "L"]))
+    
+    ## transform chroma correspondingly (relative to maximum chroma possible)
+    col@coords[, "C"] <- col@coords[, "C"]/ceiling(max_chroma(col@coords[, "H"], Lold) + 1e-8) *
+      max_chroma(col@coords[, "H"], col@coords[, "L"], floor = TRUE)
+    # check once more that resulting chroma is within appropriate bounds
+    col@coords[, "C"] <- pmin(max_chroma(col@coords[, "H"], col@coords[, "L"], floor = TRUE),
+                              pmax(0, col@coords[, "C"]))
+  } 
+  else if (space == "HLS") {
+    ## *** darkening/lightening in HLS space ***
+
+    col <- as(col, "HLS")
+    col@coords[, "L"] <- if(method == "relative") {
+      (amount >= 0) * (1 - (1 - col@coords[, "L"]) * (1 - amount)) +
+        (amount < 0) * col@coords[, "L"] * (1 + amount)
+    } else {
+      col@coords[, "L"] + amount
+    }
+    col@coords[, "L"] <- pmin(1, pmax(0, col@coords[, "L"]))
+  } else {
+    ## *** darkening/lightening in combined space ***
+    
+    colHLS <- as(col, "HLS")
+    colHLS@coords[, "L"] <- if(method == "relative") {
+      (amount >= 0) * (1 - (1 - colHLS@coords[, "L"]) * (1 - amount)) +
+        (amount < 0) * colHLS@coords[, "L"] * (1 + amount)
+    } else {
+      colHLS@coords[, "L"] + amount
+    }
+    colHLS@coords[, "L"] <- pmin(1, pmax(0, colHLS@coords[, "L"]))
+    
+    colHLSHCL <- as(as(colHLS, "RGB"), "polarLUV")
+    
+    ## convert to HCL
+    colHCL <- as(col, "polarLUV")
+    ## fix-up extreme luminance cases
+    colHCL@coords[, "L"] <- pmin(100, pmax(0, colHCL@coords[, "L"]))
+    
+    ## transform luminance
+    Lold <- colHCL@coords[, "L"]
+    colHCL@coords[, "L"] <- if(method == "relative") {
+      (amount >= 0) * (100 - (100 - Lold) * (1 - amount)) +
+        (amount < 0) * Lold * (1 + amount)
+    } else {
+      Lold + amount * 100
+    }
+    
+    colHCL@coords[, "L"] <- pmin(100, pmax(0, colHCL@coords[, "L"]))
+    colHCL@coords[, "H"] <- colHLSHCL@coords[, "H"]
+    colHCL@coords[, "C"] <- colHLSHCL@coords[, "C"]
+    
+    ## transform chroma correspondingly (relative to maximum chroma possible)
+    colHCL@coords[, "C"] <- pmin(max_chroma(colHCL@coords[, "H"], colHCL@coords[, "L"], floor = TRUE), colHCL@coords[, "C"])
+    col <- colHCL
+  }
+  
+  ## convert back to hex and add alpha again (if any)
+  col <- hex(col, fixup = fixup)
+  col[!is.na(col)] <- paste(col[!is.na(col)], alpha[!is.na(col)], sep = "")
+  return(col)
+}
+
+
+if(FALSE) {
+  ## code
+  library("colorspace")
+  
+  ## convenience demo functions
+  wheel <- function(col, radius = 1, ...)
+     pie(rep(1, length(col)), col = col, radius = radius, ...)
+  
+  pal <- function(col, border = "light gray") {
+    n <- length(col)
+    plot(0, 0, type="n", xlim = c(0, 1), ylim = c(0, 1), axes = FALSE, xlab = "", ylab = "")
+    rect(0:(n-1)/n, 0, 1:n/n, 1, col = col, border = border)
+  }
+  
+  
+  ## compare original & lightened colors
+  par(mar = rep(0, 4), mfrow = c(6, 3))
+  wheel(rainbow_hcl(12)); mtext("HLS")
+  wheel(rainbow_hcl(12)); mtext("HCL")
+  wheel(rainbow_hcl(12)); mtext("combined")
+  wheel(lightdark2(rainbow_hcl(12), amount = 0.2))
+  wheel(lightdark2(rainbow_hcl(12), amount = 0.2, space = "HCL"))
+  wheel(lightdark2(rainbow_hcl(12), amount = 0.2, space = "combined"))
+  wheel(lightdark2(rainbow_hcl(12), amount = 0.4))
+  wheel(lightdark2(rainbow_hcl(12), amount = 0.4, space = "HCL"))
+  wheel(lightdark2(rainbow_hcl(12), amount = 0.4, space = "combined"))
+  wheel(rainbow(12))
+  wheel(rainbow(12))
+  wheel(rainbow(12))
+  wheel(lightdark2(rainbow(12), amount = 0.2))
+  wheel(lightdark2(rainbow(12), amount = 0.2, space = "HCL"))
+  wheel(lightdark2(rainbow(12), amount = 0.2, space = "combined"))
+  wheel(lightdark2(rainbow(12), amount = 0.4))
+  wheel(lightdark2(rainbow(12), amount = 0.4, space = "HCL"))
+  wheel(lightdark2(rainbow(12), amount = 0.4, space = "combined"))
+  
+  ## compare original & darkened colors
+  par(mar = rep(0, 4), mfrow = c(6, 3))
+  wheel(rainbow_hcl(12)); mtext("HLS")
+  wheel(rainbow_hcl(12)); mtext("HCL")
+  wheel(rainbow_hcl(12)); mtext("combined")
+  wheel(lightdark2(rainbow_hcl(12), amount = -0.2))
+  wheel(lightdark2(rainbow_hcl(12), amount = -0.2, space = "HCL"))
+  wheel(lightdark2(rainbow_hcl(12), amount = -0.2, space = "combined"))
+  wheel(lightdark2(rainbow_hcl(12), amount = -0.4))
+  wheel(lightdark2(rainbow_hcl(12), amount = -0.4, space = "HCL"))
+  wheel(lightdark2(rainbow_hcl(12), amount = -0.4, space = "combined"))
+  wheel(rainbow(12))
+  wheel(rainbow(12))
+  wheel(rainbow(12))
+  wheel(lightdark2(rainbow(12), amount = -0.2))
+  wheel(lightdark2(rainbow(12), amount = -0.2, space = "HCL"))
+  wheel(lightdark2(rainbow(12), amount = -0.2, space = "combined"))
+  wheel(lightdark2(rainbow(12), amount = -0.4))
+  wheel(lightdark2(rainbow(12), amount = -0.4, space = "HCL"))
+  wheel(lightdark2(rainbow(12), amount = -0.4, space = "combined"))
+  
+
+  
+  ## fairly dark/colorful colors --> turn lighter
+  cl <- rainbow_hcl(5)
+  
+  ## HLS vs. HCL vs. combined --> HCL works best
+  par(mfrow = c(3, 3), mar = rep(0, 4), oma = c(0, 0, 2, 0))
+  pal(cl); mtext("HLS")
+  pal(cl); mtext("HCL")
+  pal(cl); mtext("combined")
+  pal(lightdark2(cl, 0.2))
+  pal(lightdark2(cl, 0.2, space = "HCL"))
+  pal(lightdark2(cl, 0.2, space = "combined"))
+  pal(lightdark2(cl, 0.4))
+  pal(lightdark2(cl, 0.4, space = "HCL"))
+  pal(lightdark2(cl, 0.4, space = "combined"))
+  
+  
+  ## fairly light colors --> turn darker
+  cl <- rainbow_hcl(5, l = 90, c = 35)
+  
+  ## HLS vs. HCL vs. combined --> combined works best
+  par(mfrow = c(3, 3), mar = rep(0, 4), oma = c(0, 0, 2, 0))
+  pal(cl); mtext("HLS")
+  pal(cl); mtext("HCL")
+  pal(cl); mtext("combined")
+  pal(lightdark2(cl, -0.2))
+  pal(lightdark2(cl, -0.2, space = "HCL"))
+  pal(lightdark2(cl, -0.2, space = "combined"))
+  pal(lightdark2(cl, -0.4))
+  pal(lightdark2(cl, -0.4, space = "HCL"))
+  pal(lightdark2(cl, -0.4, space = "combined"))
+  
+  
+  ## fairly light colors --> turn darker
+  ## http://stackoverflow.com/questions/30219738/is-there-a-way-to-programmatically-darken-the-color-given-rgb-values
+  cl <- c("#CDE4F3","#E7F3D3","#F7F0C7","#EFCFE5","#D0D1E7")
+  
+  ## HLS vs. HCL vs. combined --> HLS works best
+  par(mfrow = c(3, 3), mar = rep(0, 4), oma = c(0, 0, 2, 0))
+  pal(cl); mtext("HLS")
+  pal(cl); mtext("HCL")
+  pal(cl); mtext("combined")
+  pal(lightdark2(cl, -0.2))
+  pal(lightdark2(cl, -0.2, space = "HCL"))
+  pal(lightdark2(cl, -0.2, space = "combined"))
+  pal(lightdark2(cl, -0.4))
+  pal(lightdark2(cl, -0.4, space = "HCL"))
+  pal(lightdark2(cl, -0.4, space = "combined"))
+  
+  ## fairly dark colors --> turn lighter
+  ## HLS vs. HCL vs. combined --> HCL works best
+  cl <- c("#61A9D9", "#ADD668", "#E6D152", "#CE6BAF", "#797CBA")
+  par(mfrow = c(3, 3), mar = rep(0, 4), oma = c(0, 0, 2, 0))
+  pal(cl); mtext("HLS")
+  pal(cl); mtext("HCL")
+  pal(cl); mtext("combined")
+  pal(lightdark2(cl, 0.2))
+  pal(lightdark2(cl, 0.2, space = "HCL"))
+  pal(lightdark2(cl, 0.2, space = "combined"))
+  pal(lightdark2(cl, 0.4))
+  pal(lightdark2(cl, 0.4, space = "HCL"))
+  pal(lightdark2(cl, 0.4, space = "combined"))
 }
