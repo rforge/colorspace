8debf7e2183ea42e21d045834d330b4c22da0bd1
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-06-23 09:52:13 +0000

    more flexibility in controlling the plot options in contrast_ratio()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/colorspace@597 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/colorspace/R/contrast_ratio.R
+++ pkg/colorspace/R/contrast_ratio.R
@@ -12,7 +12,7 @@
 #' (see \url{https://www.w3.org/TR/WCAG21/#dfn-relative-luminance}) of the lighter and darker
 #' colors, respectively. The relative luminances are weighted sums of scaled sRGB coordinates:
 #' \code{0.2126 * R + 0.7152 * G + 0.0722 * B} where each of \code{R}, \code{G}, and \code{B}
-#' is defined as \code{ifelse(RGB <= 0.03928, RGB/12.92, ((RGB + 0.055)/1.055)^2.4 )} based on
+#' is defined as \code{ifelse(RGB <= 0.03928, RGB/12.92, ((RGB + 0.055)/1.055)^2.4)} based on
 #' the \code{RGB} coordinates between 0 and 1.
 #' 
 #' @param col vector of any of the three kind of R colors, i.e., either a color
@@ -26,6 +26,18 @@
 #' visualized by simple color swatches. Can also be a vector of length 2, indicating
 #' whether the foreground color should be visualized on the background color and/or
 #' the background color on the foreground color.
+#' @param border logical or color specification for the borders around the color swatches (only
+#' used if \code{plot = TRUE}). The default is \code{FALSE} which is equivalent to
+#' \code{"transparent"}. If \code{TRUE} the border is drawn in the same color as the text in the
+#' rectangle.
+#' @param cex numeric. Size of the text in the color color swatches (only if \code{plot = TRUE}).
+#' @param off numeric. Vertical offset between the different color swatches (only if \code{plot = TRUE}).
+#' Can also be of length 2 giving both vertical and horizontal offsets, respectively.
+#' @param mar numeric. Size of the margins around the color swatches (only if \code{plot = TRUE}).
+#' @param digits numeric. Number of digits for the contrast ratios displayed in the color swatches
+#' (only if \code{plot = TRUE})
+#' @param \dots further arguments passed to the plot of the color swatches (only if \code{plot = TRUE}).
+#'
 #' @return A numeric vector with the contrast ratios is returned (invisibly, if \code{plot} is \code{TRUE}).
 #' @seealso \code{\link[colorspace]{desaturate}}
 #' @references W3C (2018). \dQuote{Web Content Accessibility Guidelines (WCAG) 2.1.}
@@ -44,7 +56,9 @@
 #' @importFrom graphics rect
 #' @importFrom graphics text
 
-contrast_ratio <- function(col, col2 = NULL, plot = FALSE) {
+contrast_ratio <- function(col, col2 = NULL,
+  plot = FALSE, border = FALSE, cex = 2, off = 0.05, mar = rep(0.5, 4), digits = 2L, ...)
+{
   ## https://www.w3.org/TR/WCAG21/#dfn-contrast-ratio
   ## determine number of color pairs
   n <- if(is.null(col2)) length(col) - 1L else max(c(length(col), length(col2)))
@@ -67,16 +81,18 @@ contrast_ratio <- function(col, col2 = NULL, plot = FALSE) {
   ## optionally visualize
   plot <- rep_len(plot, 2L)
   if(any(plot)) {
-    opar <- par(mar = rep(0.5, 4))
+    opar <- par(mar = mar)
     on.exit(par(opar))
-    plot(0, 0, xlim = c(0, sum(plot)), ylim = c(0, n), type = "n", axes = FALSE, xlab = "", ylab = "")
+    if(identical(border, FALSE)) border <- "transparent"
+    if(length(off) == 1L) off <- c(off, 0)
+    plot(0, 0, xlim = c(0, sum(plot)), ylim = c(0, n), type = "n", axes = FALSE, xlab = "", ylab = "", ...)
     if(plot[1L]) {
-      rect(0L, 1L:n - 1L, 1L, 1L:n - 0.05, col = col, border = "transparent")
-      text(0.5, 1L:n - 0.525, format(round(ratio, digits = 2L), nsmall = 2L), cex = 2, col = col2)
+      rect(0L, 1L:n - 1L, 1L - off[2L], 1L:n - off[1L], col = col, border = if(isTRUE(border)) col2 else border)
+      text((1 - off[2L])/2, 1L:n - (1 - (1 - off[1L])/2), format(round(ratio, digits = digits), nsmall = 2L), cex = cex, col = col2)
     }
     if(plot[2L]) {
-      rect(0L + plot[1L], 0L:(n - 1L), 1L + plot[1L], 0L:(n - 1L) + 0.95, col = col2, border = "transparent")
-      text(0.5 + plot[1L], 1L:n - 0.525, format(round(ratio, digits = 2L), nsmall = 2L), cex = 2, col = col)
+      rect(0L + plot[1L], 0L:(n - 1L), 1L - off[2L] + plot[1L], 1L:n - off[1L], col = col2, border = if(isTRUE(border)) col else border)
+      text((1 - off[2L])/2 + plot[1L], 1L:n - (1 - (1 - off[1L])/2), format(round(ratio, digits = digits), nsmall = 2L), cex = cex, col = col)
     }
     invisible(ratio)
   } else {
